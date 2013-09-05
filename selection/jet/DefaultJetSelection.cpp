0c297c222abe57695e6eeb52245cd163329da16a
matt-komm <Matthias.Komm@cern.ch>
2013-09-05 13:44:19 +0200

    small improvements

--- selection/jet/DefaultJetSelection.cpp
+++ selection/jet/DefaultJetSelection.cpp
@@ -24,8 +24,6 @@ class DefaultJetSelection : public pxl::Module
     std::string _inputEventViewName;
     std::string _selectedJetName;
     bool _cleanEvent;
-    int64_t _maxJets;
-    int64_t _minJets;
 
     public:
     DefaultJetSelection() :
